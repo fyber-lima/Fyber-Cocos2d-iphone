b2713a0555a0c97956e166376c608bd407f224ea
Robert Segal <rob@getsetgames.com>
2015-01-26 23:15:30 -0500

    Fyber Vungle network adapter added

---
+++ Fyber-Cocos2d-iphone/Fyber-Cocos2d-iphone/ads/sponsorpay-sdk/NetworkAdapters/Vungle/SPVungleNetwork.h
@@ -0,0 +1,30 @@
+//
+// SPVungleNetwork.h
+//
+// Created on 13/01/14.
+// Copyright (c) 2014 Fyber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <VungleSDK/VungleSDK.h>
+#import "SPBaseNetwork.h"
+
+/**
+ Network class in charge of integrating Vungle library
+ 
+ ## Version compatibility
+ 
+ - Adapter version: 2.3.0
+ - Fyber SDK version: 7.0.3
+ - Vungle SDK version: 3.0.11
+ 
+ */
+
+@interface SPVungleNetwork : SPBaseNetwork
+
+@property (nonatomic, copy) NSString *appId;
+@property (nonatomic, copy) NSString *orientation;
+@property (nonatomic, readonly) UIInterfaceOrientationMask orientationMask;
+
+@end
---
+++ Fyber-Cocos2d-iphone/Fyber-Cocos2d-iphone/ads/sponsorpay-sdk/NetworkAdapters/Vungle/SPVungleNetwork.m
@@ -0,0 +1,106 @@
+//
+// SPVungleNetwork.m
+//
+// Created on 13/01/14.
+// Copyright (c) 2014 Fyber. All rights reserved.
+//
+
+#import "SPVungleNetwork.h"
+#import "SPLogger.h"
+#import "SPSemanticVersion.h"
+#import "SPTPNGenericAdapter.h"
+
+static NSString *const SPVungleAppId = @"SPVungleAppId";
+static NSString *const SPVungleOrientation = @"SPVungleOrientation";
+
+static const NSInteger SPVungleVersionMajor = 2;
+static const NSInteger SPVungleVersionMinor = 3;
+static const NSInteger SPVungleVersionPatch = 0;
+
+static NSString *const SPRewardedVideoAdapterClassName = @"SPVungleRewardedVideoAdapter";
+
+// Screen orientation Constatns
+NSString *const SPVungleInterfaceOrientationMaskPortrait = @"portrait";
+NSString *const SPVungleInterfaceOrientationMaskLandscapeLeft = @"landscapeLeft";
+NSString *const SPVungleInterfaceOrientationMaskLandscapeRight = @"landscapeRight";
+NSString *const SPVungleInterfaceOrientationMaskPortraitUpsideDown = @"portraitUpsideDown";
+NSString *const SPVungleInterfaceOrientationMaskLandscape = @"landscape";
+NSString *const SPVungleInterfaceOrientationMaskAll = @"all";
+NSString *const SPVungleInterfaceOrientationMaskAllButUpsideDown = @"allButUpsideDown";
+
+@interface SPVungleNetwork ()
+
+@property (nonatomic, assign) SPNetworkSupport supportedServices;
+@property (nonatomic, copy) NSString *providerName;
+@property (nonatomic, strong) SPTPNGenericAdapter *rewardedVideoAdapter;
+@end
+
+@implementation SPVungleNetwork
+
+@synthesize supportedServices;
+@synthesize rewardedVideoAdapter = _rewardedVideoAdapter;
+
+#pragma mark - Class Methods
+
++ (SPSemanticVersion *)adapterVersion
+{
+    return [SPSemanticVersion versionWithMajor:SPVungleVersionMajor minor:SPVungleVersionMinor patch:SPVungleVersionPatch];
+}
+
+#pragma mark - Initialization
+
+- (BOOL)startSDK:(NSDictionary *)data
+{
+    NSString *appId = data[SPVungleAppId];
+
+    if (!appId.length) {
+        SPLogError(@"Could not start %@ Provider. %@ empty or missing.", self.name, SPVungleAppId);
+        return NO;
+    }
+
+    self.orientation = data[SPVungleOrientation];
+
+    self.appId = appId;
+    [[VungleSDK sharedSDK] startWithAppId:self.appId];
+    return YES;
+}
+
+- (void)startRewardedVideoAdapter:(NSDictionary *)data
+{
+    Class RewardedVideoAdapterClass = NSClassFromString(SPRewardedVideoAdapterClassName);
+    if (!RewardedVideoAdapterClass) {
+        return;
+    }
+
+    id<SPRewardedVideoNetworkAdapter> vungleRewardedVideoAdapter = [[RewardedVideoAdapterClass alloc] init];
+
+    SPTPNGenericAdapter *vungleRewardedVideoAdapterWrapper = [[SPTPNGenericAdapter alloc] initWithVideoNetworkAdapter:vungleRewardedVideoAdapter];
+    vungleRewardedVideoAdapter.delegate = vungleRewardedVideoAdapterWrapper;
+
+    self.rewardedVideoAdapter = vungleRewardedVideoAdapterWrapper;
+
+    [super startRewardedVideoAdapter:data];
+}
+
+#pragma mark - Helper methods
+
+- (UIInterfaceOrientationMask)orientationMask
+{
+    if ([self.orientation isEqualToString:SPVungleInterfaceOrientationMaskPortrait])
+        return UIInterfaceOrientationMaskPortrait;
+    if ([self.orientation isEqualToString:SPVungleInterfaceOrientationMaskLandscapeLeft])
+        return UIInterfaceOrientationMaskLandscapeLeft;
+    if ([self.orientation isEqualToString:SPVungleInterfaceOrientationMaskLandscapeRight])
+        return UIInterfaceOrientationMaskLandscapeRight;
+    if ([self.orientation isEqualToString:SPVungleInterfaceOrientationMaskPortraitUpsideDown])
+        return UIInterfaceOrientationMaskPortraitUpsideDown;
+    if ([self.orientation isEqualToString:SPVungleInterfaceOrientationMaskLandscape])
+        return UIInterfaceOrientationMaskLandscape;
+    if ([self.orientation isEqualToString:SPVungleInterfaceOrientationMaskAll])
+        return UIInterfaceOrientationMaskAll;
+    if ([self.orientation isEqualToString:SPVungleInterfaceOrientationMaskAllButUpsideDown])
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    return UIInterfaceOrientationMaskAll;
+}
+
+@end
